f51d818360a3ff50321821891f94c94a42ce90aa
alexeybokov <lexabokov18@gmail.com>
2018-06-13 22:25:22 +0300

    add game and test for check_result

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,5 +1,76 @@
-require "codebreaker/version"
+require 'codebreaker/version'
 
 module Codebreaker
-  # Your code goes here...
+  class Game
+
+    def initialize
+      @secret_code = []
+      @user_code = []
+      @max_attempts = 8
+    end
+
+    def start
+      4.times do
+        @secret_code.push(rand(1...6))
+      end
+    end
+
+    def run_game
+      @max_attempts.times do |current_round|
+        puts "Round: #{current_round + 1}\n"
+        user_input
+        return won if won?
+        check_result
+        puts "#{@max_attempts - current_round - 1} attempts left\n"
+      end
+      puts "You're looser!"
+    end
+
+    def won?
+      @user_code == @secret_code
+    end
+
+    def won
+      puts 'You won!'
+    end
+
+    NUMBERS_TO_WORDS = {
+        0 => 'first',
+        1 => 'second',
+        2 => 'third',
+        3 => 'fourth'
+    }.freeze
+
+    def number_valid?(number)
+      number >= 1 && number <= 6
+    end
+
+    def user_input
+      @user_code = []
+      4.times do |index|
+        puts "Enter #{NUMBERS_TO_WORDS[index]} number from 1 to 6:"
+        number = 0
+        while !number_valid?(number) do
+          number = gets.chomp.to_i
+          puts 'Use only numbers from 1 to 6!' unless number_valid?(number)
+        end
+        @user_code.push(number)
+      end
+      puts "Your codes: #{@user_code.join(' ')}"
+    end
+
+    def check_result
+      result = []
+      common = @secret_code & @user_code
+      @user_code.each_with_index.map do |code, index|
+        if code == @secret_code[index]
+          result.push '+'
+        elsif common.include?(code)
+          result.push '-'
+        end
+      end
+      puts "Round result: #{result.join(' ')}"
+      result
+    end
+  end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,19 +2,69 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
-    context '#start' do
+    context '#initialize' do
       let(:game) { Game.new }
       before do
         game.start
       end
+
       it 'saves secret code' do
         expect(game.instance_variable_get(:@secret_code)).not_to be_empty
       end
+
       it 'saves 4 numbers secret code' do
         expect(game.instance_variable_get(:@secret_code).size).to eq 4
       end
+
       it 'saves secret code with numbers from 1 to 6' do
         expect(game.instance_variable_get(:@secret_code).to_s).to match(/[1-6]+/)
+      end
+
+      it 'max attempt set to 8' do
+        expect(game.instance_variable_get(:@max_attempts)).to eq 8
+      end
+    end
+
+    context '#check_result' do
+      let(:game) { Game.new }
+      before do
+        game.start
+        game.instance_variable_set :@secret_code, [6, 1, 3, 4]
+      end
+
+      it 'should return +' do
+        game.instance_variable_set :@user_code, [6, 2, 5, 2]
+        expect(game.check_result).to eq(["+"])
+      end
+
+      it 'should return ++' do
+        game.instance_variable_set :@user_code, [6, 1, 5, 2]
+        expect(game.check_result).to eq(["+", "+"])
+      end
+
+      it 'should return +++' do
+        game.instance_variable_set :@user_code, [6, 1, 5, 4]
+        expect(game.check_result).to eq(["+", "+", "+"])
+      end
+
+      it 'should return -' do
+        game.instance_variable_set :@user_code, [4, 2, 5, 2]
+        expect(game.check_result).to eq(["-"])
+      end
+
+      it 'should return --' do
+        game.instance_variable_set :@user_code, [1, 3, 5, 2]
+        expect(game.check_result).to eq(["-", "-"])
+      end
+
+      it 'should return ---' do
+        game.instance_variable_set :@user_code, [4, 3, 6, 2]
+        expect(game.check_result).to eq(["-", "-", "-"])
+      end
+
+      it 'should return --++' do
+        game.instance_variable_set :@user_code, [1, 6, 3, 4]
+        expect(game.check_result).to eq(["-", "-", "+", "+"])
       end
     end
   end
