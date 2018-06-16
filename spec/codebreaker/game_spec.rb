b4cd7d069f107acfac7110101439ebdd4f3d5899
alexeybokov <lexabokov18@gmail.com>
2018-06-16 19:49:47 +0300

    add and update test for codebreaker

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,70 +2,105 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
-    context '#initialize' do
-      let(:game) { Game.new }
-      before do
-        game.start
+    subject { Game.new }
+
+    context 'user_input' do
+      it 'accepts 4 valid numbers' do
+        $stdin = StringIO.new("1\n2\n3\n4\n")
+        expect do
+          subject.validate_user_input
+          $stdin = STDIN
+        end.to change { subject.instance_variable_get(:@user_code) }.from([]).to([1, 2, 3, 4])
       end
 
+      it 'accepts 4 valid numbers if some invalid provided' do
+        $stdin = StringIO.new("1\n8\n3\n4\n3\n")
+        expect do
+          subject.validate_user_input
+          $stdin = STDIN
+        end.to change { subject.instance_variable_get(:@user_code) }.from([]).to([1, 3, 4, 3])
+      end
+    end
+
+    context '#initialize' do
       it 'saves secret code' do
-        expect(game.instance_variable_get(:@secret_code)).not_to be_empty
+        expect(subject.instance_variable_get(:@secret_code)).not_to be_empty
       end
 
       it 'saves 4 numbers secret code' do
-        expect(game.instance_variable_get(:@secret_code).size).to eq 4
+        expect(subject.instance_variable_get(:@secret_code).size).to eq 4
       end
 
       it 'saves secret code with numbers from 1 to 6' do
-        expect(game.instance_variable_get(:@secret_code).to_s).to match(/[1-6]+/)
-      end
-
-      it 'max attempt set to 8' do
-        expect(game.instance_variable_get(:@max_attempts)).to eq 8
+        expect(subject.instance_variable_get(:@secret_code)).to consist_from(Codebreaker::Game::SECRET_CODE_RANGE)
       end
     end
 
     context '#check_result' do
-      let(:game) { Game.new }
       before do
-        game.start
-        game.instance_variable_set :@secret_code, [6, 1, 3, 4]
+        subject.instance_variable_set :@secret_code, [6, 1, 3, 4]
       end
 
-      it 'should return +' do
-        game.instance_variable_set :@user_code, [6, 2, 5, 2]
-        expect(game.check_result).to eq(["+"])
+      it 'when first number the same' do
+        subject.instance_variable_set :@user_code, [6, 2, 5, 2]
+        expect(subject.prepare_result).to eq(['+'])
       end
 
-      it 'should return ++' do
-        game.instance_variable_set :@user_code, [6, 1, 5, 2]
-        expect(game.check_result).to eq(["+", "+"])
+      it 'when two numbers match' do
+        subject.instance_variable_set :@user_code, [6, 2, 3, 2]
+        expect(subject.prepare_result).to eq(['+', '+'])
       end
 
-      it 'should return +++' do
-        game.instance_variable_set :@user_code, [6, 1, 5, 4]
-        expect(game.check_result).to eq(["+", "+", "+"])
+      it 'when three numbers match' do
+        subject.instance_variable_set :@user_code, [6, 1, 5, 4]
+        expect(subject.prepare_result).to eq(["+", "+", "+"])
       end
 
-      it 'should return -' do
-        game.instance_variable_set :@user_code, [4, 2, 5, 2]
-        expect(game.check_result).to eq(["-"])
+      it 'when number is guessed but position is incorrect' do
+        subject.instance_variable_set :@user_code, [4, 2, 5, 2]
+        expect(subject.prepare_result).to eq(["-"])
       end
 
-      it 'should return --' do
-        game.instance_variable_set :@user_code, [1, 3, 5, 2]
-        expect(game.check_result).to eq(["-", "-"])
+      it 'when two numders is guessed but position is incorrect' do
+        subject.instance_variable_set :@user_code, [1, 3, 5, 2]
+        expect(subject.prepare_result).to eq(["-", "-"])
       end
 
-      it 'should return ---' do
-        game.instance_variable_set :@user_code, [4, 3, 6, 2]
-        expect(game.check_result).to eq(["-", "-", "-"])
+      it 'when three numders is guessed but position is incorrect' do
+        subject.instance_variable_set :@user_code, [4, 3, 6, 2]
+        expect(subject.prepare_result).to eq(["-", "-", "-"])
       end
 
-      it 'should return --++' do
-        game.instance_variable_set :@user_code, [1, 6, 3, 4]
-        expect(game.check_result).to eq(["-", "-", "+", "+"])
+      it 'when all numbers guessed but two numbers have incorrect position' do
+        subject.instance_variable_set :@user_code, [1, 6, 3, 4]
+        expect(subject.prepare_result).to eq(["-", "-", "+", "+"])
       end
     end
+
+    context '#get_hint' do
+      it 'should return one number of secret code' do
+        subject.instance_variable_set :@secret_code, [6, 1, 3, 4]
+        expected_array = ['***4', '**3*', '*1**', '6***']
+          expect(expected_array).to include(subject.get_hint)
+      end
+
+      it 'hint can be taken once' do
+        subject.get_hint
+        expect(subject.instance_variable_get(:@hint)).to be_falsey
+      end
+    end
+
+    context '#won' do
+      it 'sets game result into win' do
+        expect(@user_code == @secret_code).to be_truthy
+      end
+    end
+
+    context '#lost' do
+      it 'should return message when game over' do
+        expect(subject.send(:lost)).to be_falsey
+      end
+    end
+
   end
 end
